e05595b58de746c74d5738977bced4542bda8e0f
arseniy-poltev <hgh141592_71@outlook.com>
2020-05-25 13:18:38 +0700

    Add Loading spinner function

--- CustomTreeview/CustomTree.js
+++ CustomTreeview/CustomTree.js
@@ -645,6 +645,26 @@ export default class CustomTree extends React.Component {
     this.setState({ treeState: saveString });
   }
 
+  displayLoadingSpinner = () => {
+    return (
+      this.state.isLoading &&
+      <div className="sk-circle">
+        <div className="sk-circle1 sk-child"></div>
+        <div className="sk-circle2 sk-child"></div>
+        <div className="sk-circle3 sk-child"></div>
+        <div className="sk-circle4 sk-child"></div>
+        <div className="sk-circle5 sk-child"></div>
+        <div className="sk-circle6 sk-child"></div>
+        <div className="sk-circle7 sk-child"></div>
+        <div className="sk-circle8 sk-child"></div>
+        <div className="sk-circle9 sk-child"></div>
+        <div className="sk-circle10 sk-child"></div>
+        <div className="sk-circle11 sk-child"></div>
+        <div className="sk-circle12 sk-child"></div>
+      </div>
+    )
+  }
+
   render() {
     const {
       treeData,
@@ -683,22 +703,7 @@ export default class CustomTree extends React.Component {
               showOnlyMatches={showOnlyMatches}
             />
           </Draggable>
-          {this.state.isLoading &&
-            <div className="sk-circle">
-              <div className="sk-circle1 sk-child"></div>
-              <div className="sk-circle2 sk-child"></div>
-              <div className="sk-circle3 sk-child"></div>
-              <div className="sk-circle4 sk-child"></div>
-              <div className="sk-circle5 sk-child"></div>
-              <div className="sk-circle6 sk-child"></div>
-              <div className="sk-circle7 sk-child"></div>
-              <div className="sk-circle8 sk-child"></div>
-              <div className="sk-circle9 sk-child"></div>
-              <div className="sk-circle10 sk-child"></div>
-              <div className="sk-circle11 sk-child"></div>
-              <div className="sk-circle12 sk-child"></div>
-            </div>
-          }
+          {this.displayLoadingSpinner()}
           {this.state.initialTreeData !== null &&
             <div className="tree-content text-white" style={styles.treeContent}>
               <DndProvider backend={dndBackend}>
